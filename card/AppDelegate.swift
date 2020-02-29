fccc42f7592205e3528767f18f450ac5172dc189
Tiago Martinho <t.martinho@icloud.com>
2020-02-29 10:56:30 +0100

    Initial Commit

---
+++ card/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  card
+//
+//  Created by Tiago Martinho on 29/02/2020.
+//  Copyright © 2020 Tiago Martinho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ card/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  card
+//
+//  Created by Tiago Martinho on 29/02/2020.
+//  Copyright © 2020 Tiago Martinho. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
