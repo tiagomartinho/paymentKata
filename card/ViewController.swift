23120655e9112eccfe0ae48e079370c613de0e21
Tiago Martinho <t.martinho@icloud.com>
2020-02-29 11:29:49 +0100

    Hook presenter with view

--- card/ViewController.swift
+++ card/ViewController.swift
@@ -5,9 +5,14 @@ class ViewController: NSViewController {
     @IBOutlet weak var switchLidlPay: NSSwitch!
     @IBOutlet weak var descriptinTextField: NSTextField!
 
+    lazy var presenter = CardPresenter(view: self)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let paymentOption = PaymentOption(selected: false)
+//        let paymentOption: PaymentOption? = nil
+        let card = Card(paymentOption: paymentOption)
+        presenter.load(with: card)
     }
 
     override var representedObject: Any? {
@@ -15,5 +20,13 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    @IBAction func swithcPayToggl(_ sender: NSSwitch) {
+    }
 }
 
+extension ViewController: CardView {
+    func showPaymentOption() {
+        switchLidlPay.isHidden = false
+        descriptinTextField.isHidden = false
+    }
+}
